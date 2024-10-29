9389568f1ec27630353615ded602bcb35172d54a
Iuliia Novikova <julynovik2000@gmail.com>
2024-10-29 03:04:52 +0300

    Update Pharmacogenomics.py

--- Pharmacogenomics.py
+++ Pharmacogenomics.py
@@ -10,39 +10,32 @@ from openpyxl.styles import Font
 def load_vcf_from_string(vcf_string):
     vcf_reader = vcf.Reader(StringIO(vcf_string))
     vcf_records = []
-    
     for record in vcf_reader:
         vcf_records.append(record)
-    
     return vcf_records
 
 def load_vcf_chrom_pos_bcf(vcf_file, chrom, pos):
     command = ['bcftools', 'view', '-r', f"{chrom}:{pos}", vcf_file]
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
     if result.returncode != 0:
         print(f"Ошибка в bcftools: {result.stderr.decode()}")
         return None
-    
     vcf_output = result.stdout.decode()
     vcf_records = load_vcf_from_string(vcf_output)
-    
     return vcf_records
 
 def annotate_genotypes(excel_file, vcf_file, output_file):
-    dfs = pd.read_excel(excel_file, header=None, sheet_name=None, engine='openpyxl')
+    dfs = pd.read_excel(excel_file, sheet_name=None, engine='openpyxl')
 
     results_dict = {}
 
     for disease, df in dfs.items():
+        column_names = list(df.columns) + ['Genotype', 'DP']
+
         results = []
         for _, row in df.iterrows():
-            Chrom = row[0].strip()
-            Pos = int(row[1])
-            Rs_id = row[2].strip()
-            Gene = row[3].strip()
-            Name = row[4].strip()
-            Ref = row[5].strip()
+            Chrom = row.iloc[0].strip()
+            Pos = int(row.iloc[1])
 
             vcf_records = load_vcf_chrom_pos_bcf(vcf_file, Chrom, Pos)
 
@@ -63,17 +56,16 @@ def annotate_genotypes(excel_file, vcf_file, output_file):
                 except Exception as e:
                     dp = '--'
 
-                results.append((Chrom, Pos, Rs_id, Gene, Name, Ref, genotype_str, dp))
+                results.append(list(row) + [genotype_str, dp])
             else:
-                results.append((Chrom, Pos, Rs_id, Gene, Name, Ref, '--', '--'))
+                results.append(list(row) + ['--', '--'])
 
-        columns = ['Chrom', 'Pos', 'Rs_id', 'Gene', 'Name', 'Ref', 'Genotype', 'DP']
-        results_df = pd.DataFrame(results, columns=columns)
+        results_df = pd.DataFrame(results, columns=column_names)
         results_dict[disease] = results_df
 
     with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
         for sheet_name, df in results_dict.items():
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
+            df.to_excel(writer, sheet_name=sheet_name, index=False, header=True)
 
     workbook = load_workbook(output_file)
     for sheet_name in results_dict.keys():
